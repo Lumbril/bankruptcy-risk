b6bdfe54221ea4b6f224fed451c78275fc5fceb7
Vitaliy Koltsov <vitaliy.koltsov.2002@gmail.com>
2024-03-04 10:45:43 +0300

    added open file

--- Application/src/main/java/com/example/application/controllers/Controller.java
+++ Application/src/main/java/com/example/application/controllers/Controller.java
@@ -1,14 +1,22 @@
 package com.example.application.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private TextField pathToFile;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onOpenExplorerClick() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("excel files (*.xls, *.xlsx)", "*.xlsx", "*.xls");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showOpenDialog(null);
+
+        pathToFile.setText(file.getAbsoluteFile().getAbsolutePath());
     }
 }
